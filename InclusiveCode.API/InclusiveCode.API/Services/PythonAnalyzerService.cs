030d035697fb6c2466db185a4a688eeca7d7e5e3
Murilo013 <muriloalessioamaral@gmail.com>
2026-02-27 20:50:14 -0300

    Add files

---
+++ InclusiveCode.API/InclusiveCode.API/Services/PythonAnalyzerService.cs
@@ -0,0 +1,42 @@
+﻿using InclusiveCode.API.Models;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+namespace InclusiveCode.API.Services
+{
+    public class PythonAnalyzerService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PythonAnalyzerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<PythonAnalyzerResponse?> RunAnalysis(string repoUrl)
+        {
+            var body = new { url = repoUrl };
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(body),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _httpClient.PostAsync(
+                "http://127.0.0.1:8000/analyze",
+                content
+            );
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            return JsonSerializer.Deserialize<PythonAnalyzerResponse>(responseString, options);
+        }
+    }
+}
